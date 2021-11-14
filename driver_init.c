3dcd99248ede1ec6ec3776ea82ec0c716d5343f6
Nguyễn Minh Thiện <92174712+minhthien2003@users.noreply.github.com>
2021-11-14 23:20:20 +0700

    Create driver_init.c
    driver basic init

---
+++ driver_init.c
@@ -0,0 +1,17 @@
+#include<linux/kernel.h>
+#include<linux/init.h>
+#include<linux/module.h>
+static int hello_init(void)
+
+{
+
+    printk(KERN_ALERT "Khoi tao thanh cong\n");
+
+return 0;
+}
+static void hello_exit(void)
+{
+    printk(KERN_ALERT "Ket thuc thanh cong\n");
+}
+module_init(hello_init);
+module_exit(hello_exit);
